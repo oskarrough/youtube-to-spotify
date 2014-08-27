a32abd095946d2f83b4d21992f7218e6ff74502c
Oskar Rough <oskarrough@gmail.com>
2014-08-27 15:52:27 +0200

    Add ember beta (no script tags juhu)

--- app/scripts/yts-ember.js
+++ app/scripts/yts-ember.js
@@ -1,23 +1,40 @@
+/**
+ * Start and namespace the Ember application
+ */
 window.App = Ember.Application.create();
 
-App.PlaylistModel = Ember.Object.extend({
-	id: null,
-	title: 'model title here',
-	total: 0,
-	analyzed: 0,
-	matched: 0,
-	failed: 0,
-	progress: 0,
-	matchedPercentage: 0
+// Use the fixture adapter because we don't have a real REST api
+App.ApplicationAdapter = DS.FixtureAdapter;
+
+// App.Playlists.FIXTURES = [
+// 	{ id: 1, firstName: 'Trek', lastName: 'Glowacki' },
+// 	{ id: 2, firstName: 'Tom' , lastName: 'Dale'     }
+// ];
+
+/**
+ * Models
+ */
+App.Playlist = DS.Model.extend({
+	title: DS.attr('string'),
+	firstName: DS.attr('string'),
+	birthday:  DS.attr('date'),
+	tracks: DS.hasMany('track')
+	// fullName: function() {
+	// 	return this.get('firstName') + ' ' + this.get('lastName');
+	// }.property('firstName', 'lastName')
 });
 
-var playlist = App.PlaylistModel.create();
+App.Track = DS.Model.extend({
+	playlist: DS.belongsTo('playlist'),
+	matched: DS.attr('boolean'),
+	failed: DS.attr('boolean')
+});
 
 App.Router.map(function() {
-  this.resource('convert');
+	this.resource('convert');
 });
 
-// App.ApplicationRoute = Ember.Route.extend({});
+App.ApplicationRoute = Ember.Route.extend({});
 
 App.IndexRoute = Ember.Route.extend({
 	redirect: function() {
@@ -26,13 +43,20 @@ App.IndexRoute = Ember.Route.extend({
 });
 
 App.ConvertRoute = Ember.Route.extend({
-	setupController: function(controller, song) {
-		controller.set('model', playlist);
+	model: function() {
+		 this.store.push('playlist', {
+			id: 1,
+			title: "Fewer Moving Parts"
+		 });
+	},
+	setupController: function(controller, playlist) {
+		controller.set('model', this.store.find('playlist', 1));
 	}
 });
 
 App.ConvertController = Ember.ObjectController.extend({
 	isConverting: false,
+	progress: 0,
 
 	init: function() {
 		var self = this;
@@ -48,6 +72,19 @@ App.ConvertController = Ember.ObjectController.extend({
 		// 		$('.Playlist-submit').removeClass('is-visible');
 		// 	}
 		// });
+	},
+
+	createPlaylist: function() {
+		var store = this.store;
+
+		var playlist = store.createRecord('playlist', {
+		  title: 'Rails is Omakase',
+		  body: 'Lorem ipsum'
+		});
+
+		store.find('user', 1).then(function(user) {
+		  playlist.set('author', user);
+		});
 	},
 
 	actions: {
