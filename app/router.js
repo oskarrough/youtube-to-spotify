d92c6d9179763277d275465bceed1e128e5e1445
Oskar Rough <oskarrough@gmail.com>
2015-04-16 23:47:41 +0200

    Update setup again

--- app/router.js
+++ app/router.js
@@ -2,14 +2,12 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-	location: config.locationType
+  location: config.locationType
 });
 
-Router.map(function() {
+export default Router.map(function() {
 	this.route('about');
 	this.resource('playlists', { path: ':id' }, function() {
 		// this.route('items');
 	});
 });
-
-export default Router;
